9701694b499b92e57b8e6ec10a86c1c1adb2e71a
sarmaakondi <sarmaakondivnm@icloud.com>
2024-10-13 11:43:56 +1100

    Update results view to use generic DetailView

--- polls/views.py
+++ polls/views.py
@@ -20,9 +20,9 @@ class DetailView(generic.DetailView):
     template_name = "polls/detail.html"
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 
 def vote(request, question_id):
