27f312bac5c30648beca34d82cabb1e766a4f464
sarmaakondi <sarmaakondivnm@icloud.com>
2024-10-13 11:31:12 +1100

    Remove unused import(s)

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.db.models import F
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 
