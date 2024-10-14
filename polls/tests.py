4f23b8d05d4a3131e33c532787c8e1baad32ee22
sarmaakondi <sarmaakondivnm@icloud.com>
2024-10-14 12:02:28 +1100

    Create test for no_question

--- polls/tests.py
+++ polls/tests.py
@@ -29,3 +29,11 @@ class QuestionModelTests(TestCase):
 def create_question(question_text, days):
     time = timezone.now() + datetime.timedelta(days=days)
     return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_question(self):
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerySetEqual(response.context["latest_question_list"], [])
