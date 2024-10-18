4daf1eaf9666b14d599ac109c2f141aa2b073ed9
sarmaakondi <sarmaakondivnm@icloud.com>
2024-10-18 11:19:01 +1100

    Create and update INTERNAL_IPS in settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,3 +126,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+INTERNAM_IPS = [
+    "127.0.0.1",
+]
